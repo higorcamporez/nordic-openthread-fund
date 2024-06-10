a474c82a20c80de21bee1cde5d7b37077fce4998
Higor Camporez <higorcamporez@gmail.com>
2024-06-10 13:19:29 -0300

    remoção pasta build coap

--- lesson-3/coap-server-example/build/image_preload.cmake
+++
@@ -1,9 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dongle_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "e:/projects/estagio/openthread-fund/cli_puro;e:/projects/estagio/openthread-fund/cli-coap;E:/Programas/ncs/v2.3.0/nrf;E:/Programas/ncs/v2.3.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "e:/Projects/estagio/openthread-fund/lessons/nordic-openthread-fund/aula-3/coap-server-example/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "boards/usb_nrf52840dongle_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "boards/usb_nrf52840dongle_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(OVERLAY_CONFIG "boards/usb_nrf52840dongle_nrf52840.conf" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "E:\Programas\ncs\toolchains\v2.3.0\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
